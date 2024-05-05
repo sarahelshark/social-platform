461f2eb48bf1dffee1593d92eea644e956520d22
sarahelshark <sarahelsh183@gmail.com>
2024-05-05 17:20:01 +0200

    delete
    oop page to test models

--- layout/header.php
+++ layout/header.php
@@ -17,9 +17,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Friends</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../oop.php">OOP</a>
-                </li>
+                
             </ul>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
